c912585f05e31b3ec413507f1d0d42f54ace9847
eastsideslc <mcsnet@yahoo.com>
2015-02-07 13:05:05 -0700

    Latest updates version 1.2
    Latest updates version 1.2

--- Redhat2Cent.sh
+++ Redhat2Cent.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# written by eastsideslc 2-7-2015. Version 1.1
+# written by eastsideslc 2-7-2015. Version 1.2
+
+tarloc=/tmp/Redhat2Cent
 
 remove_RHN() {
 echo "The next step will remove this server from the RHN service and remove all Redhat packages"
@@ -21,30 +23,22 @@ echo "Clearing yum cache..."
 echo
 yum clean all
 echo "cloning latest Redhat2Cent from github.  These files will reside in /tmp/Redhat2Cent/centos_migration/"
-git clone https://github.com/eastsideslc/Redhat2Cent/ /tmp/Redhat2Cent/
-chmod -R 744 /tmp/Redhat2Cent/
-if [ -f /tmp/Redhat2Cent/centos_migration/centos_migration_files.tgz ];
-        then
-                mkdir /tmp/centos_migration
-                tar xzvf /tmp/Redhat2Cent/centos_migration/centos_migration_files.tgz -C /tmp/centos_migration/
-        else
-                echo "tarball not found in /tmp/Redhat2Cent/centos_migration/centos_migration_files.tgz"
-                exit
-fi
+git clone https://github.com/eastsideslc/Redhat2Cent/ $tarloc
+tar xzvf $tarloc/centos_migration_files.tgz -C $tarloc
 echo "Import GPG key..."
-rpm --import /tmp/centos_migration/RPM-GPG-KEY-CentOS-5
+rpm --import $tarloc/RPM-GPG-KEY-CentOS-5
 echo "Installing fastestmirror module for yum..."
-rpm -ivh /tmp/centos_migration/yum-fastestmirror-1.1.16-21.el5.centos.noarch.rpm
+rpm -ivh $tarloc/yum-fastestmirror-1.1.16-21.el5.centos.noarch.rpm
 echo "Installing CentOS Release Notes RPM"
-rpm -Uvh --force /tmp/centos_migration/centos-release-notes-5.10-0.i386.rpm
+rpm -Uvh --force $tarloc/centos-release-notes-5.10-0.i386.rpm
 echo "Installing CentOS Release RPM"
-rpm -Uvh --force /tmp/centos_migration/centos-release-5-10.el5.centos.i386.rpm
+rpm -Uvh --force $tarloc/centos-release-5-10.el5.centos.i386.rpm
 echo "Installing yum 3.2.22-40 RPM"
-rpm -Uvh --force /tmp/centos_migration/yum-3.2.22-40.el5.centos.noarch.rpm
+rpm -Uvh --force $tarloc/yum-3.2.22-40.el5.centos.noarch.rpm
 echo "Installing yum updatesd 0.9-5 RPM"
-rpm -Uvh --force /tmp/centos_migration/yum-updatesd-0.9-5.el5.noarch.rpm
+rpm -Uvh --force $tarloc/yum-updatesd-0.9-5.el5.noarch.rpm
 echo "Installing EPEL 5.4 repo..."
-rpm -ivh /tmp/centos_migration/epel-release-5-4.noarch.rpm
+rpm -ivh $tarloc/epel-release-5-4.noarch.rpm
 echo "Running yum update..."
 yum -y update
 if [ ! -f /usr/share/gdm/themes/RHEL ];
@@ -71,11 +65,12 @@ if [ `whoami` = root ]; then echo "Running as root...";else echo "you must be ro
 echo "Determining Redhat version..."
 if [ -f /usr/bin/lsb_release ];
         then
-                OSVER=`lsb_release -rs | cut -f1 -d.`
+                osver=`lsb_release -rs | cut -f1 -d.`
         else
                 if [ -f /etc/issue ];
                                         then
-                                                OSVER=`egrep -o '[0-9.]{1,}' /etc/issue|awk -F . '{print $1}'`
+                                                osver=`egrep -o '[0-9.]{1,}' /etc/issue|awk -F . '{print $1}'`
+                                                echo $osver
                                         else
                                                 echo "This OS is not Redhat 5 or 6. Exiting."
                                                 exit
@@ -84,12 +79,12 @@ fi
 
 echo "Do you want to continue and migrate your system? type yes to continue"
 read readvar
-if [ $readvar = "yes" ] && [ $OSVER -eq 5 ];
+if [ $readvar = "yes" ] && [ $osver -eq 5 ];
         then echo "Migration to CentOS from Redhat 5 will start now..."
                 remove_RHN
                 migratecent
 
-elif [ $readvar = "yes" ] && [ $OSVER -eq 6 ];
+elif [ $readvar = "yes" ] && [ $osver -eq 6 ];
         then
                 echo "Migration to CentOS from Redhat 6 will start now..."
                 remove_RHN_6
